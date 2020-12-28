c091cce8bc975668eea58ce36137745d9b35089b
fahrul23 <ihsanfahrul23@gmail.com>
2020-12-28 21:40:37 +0700

    Basic Routing

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/',function(req,res){
     res.send('Hello Word');
 });
 
+app.get('/about',function(req,res){
+    res.send('Halaman About');
+})
+
+
 app.listen(4000,function(){
     console.log('Server Is Okay');
 })
